e66810876fb6589c849800d529ee26c16119fc88
andre-fajar-n <andrenugroho395@gmail.com>
2020-12-22 17:04:41 +0700

    edit format bson and json

--- model/todo.go
+++ model/todo.go
@@ -9,9 +9,9 @@ import (
 type Todo struct {
 	ID        primitive.ObjectID `bson:"_id" json:"id,omitempty"`
 	Task      string
-	IsDone    bool
+	IsDone    bool `bson:"is_done" json:"is_done"`
 	Username  string
 	Deadline  time.Time
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
 }
